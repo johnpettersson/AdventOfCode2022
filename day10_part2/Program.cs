62f360a26a3ce9f409be956b5f44efa8e3d21088
John Pettersson <john.pettersson.vgr@gmail.com>
2022-12-10 15:53:24 +0100

    Day 10 Part 2

---
+++ day10_part2/Program.cs
@@ -0,0 +1,50 @@
+﻿var lines = File.ReadAllLines("input.txt").ToList();
+
+int cycle = 0;
+int spriteX = 1;
+
+var result = new List<string> { "", "", "", "", "", "" };
+
+foreach(var line in lines)
+{
+    string[] parts = line.Split();
+    string command = parts[0];
+
+    if(command == "noop")
+    {
+        updateCycle();
+    }
+    else if(command == "addx")
+    {
+        updateCycle();
+        updateCycle();
+        spriteX += int.Parse(parts[1]);
+    }
+}
+
+foreach(var l in result)
+{
+    foreach(char c in l)
+    {
+        if(c == '#')
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+        else
+            Console.BackgroundColor = ConsoleColor.Black;
+
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+
+void updateCycle()
+{
+    cycle += 1;
+
+    int lineIndex = (cycle-1) / 40;
+    int currentX = cycle % 40;
+
+    if(currentX == spriteX || currentX == spriteX+1 || currentX == spriteX+2) 
+        result[lineIndex] += "#";
+    else
+        result[lineIndex] += ".";
+}
